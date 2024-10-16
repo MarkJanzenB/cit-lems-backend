adb2f4c3da269414d6e2591d285d33ac23590032
NifaNii <jcarlsabejon@gmail.com>
2024-10-16 17:47:50 +0800

    Added crossorigin

--- src/main/java/com/capstone/LEMS/Controller/UserController.java
+++ src/main/java/com/capstone/LEMS/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.capstone.LEMS.Controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import com.capstone.LEMS.Service.UserService;
 
 @RestController
 @RequestMapping("/user")
+@CrossOrigin
 public class UserController {
 	@Autowired
 	UserService userserv;
