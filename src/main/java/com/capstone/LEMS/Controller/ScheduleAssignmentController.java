db6bdd598f531d81bbc8f138e25129f7fb36e89f
NifaNii <jcarlsabejon@gmail.com>
2024-11-04 13:28:56 +0800

    -Added Schedule Assignment Entity

---
+++ src/main/java/com/capstone/LEMS/Controller/ScheduleAssignmentController.java
@@ -0,0 +1,42 @@
+package com.capstone.LEMS.Controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.capstone.LEMS.Entity.ScheduleAssignmentEntity;
+import com.capstone.LEMS.Service.ScheduleAssignmentService;
+
+@RestController
+@RequestMapping("/scheduleassignment")
+@CrossOrigin
+public class ScheduleAssignmentController {
+	@Autowired
+	ScheduleAssignmentService saserv;
+	
+	
+	//for testing purposes
+	@GetMapping("/message")
+	public String testMessage() {
+		return "ScheduleAssignmentController is working";
+	}
+	
+	//for adding new schedule assignment
+	@PostMapping("/insertscheduleassignment")
+	public ScheduleAssignmentEntity insertScheduleAssignment(@RequestBody ScheduleAssignmentEntity sa) {
+		return saserv.insertScheduleAssignment(sa);
+	}
+	
+	
+	//get all schedule assignment
+	@GetMapping("/getallscheduleassignment")
+	public List<ScheduleAssignmentEntity> getAllScheduleAssignment(){
+		return saserv.getAllScheduleAssignment();
+	}
+}
