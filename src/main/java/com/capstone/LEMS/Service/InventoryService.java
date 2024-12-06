2824d56620aa49b34c37c51c4dad2dcd76d2870e
dtpco <donnalyn.topacio@cit.edu>
2024-12-07 00:55:15 +0800

    Commited by: TOPACIO, DONNALYN T. -resolved an issue on fixated error(Itemname already exists) when editing an item details in inventory.

--- src/main/java/com/capstone/LEMS/Service/InventoryService.java
+++ src/main/java/com/capstone/LEMS/Service/InventoryService.java
@@ -12,75 +12,110 @@ import java.util.List;
 import java.util.Optional;
 @Service
 public class InventoryService {
-    @Autowired
-    InventoryRepository inventoryRepository;
-    public List<InventoryEntity> getAllInventory() {
-        return inventoryRepository.findAll();
-    }
+	@Autowired
+	InventoryRepository inventoryRepository;
 
-    public ResponseEntity<?> addInventory(InventoryEntity inventory) {
-    	InventoryEntity inventoryfromdb = inventoryRepository.findByNameIgnoreCase(inventory.getName());
-    	
-    	if(inventoryfromdb != null) {
-    		return ResponseEntity
-    				.status(HttpStatus.CONFLICT) // 409
-    				.body(inventory.getName() + " Inventory already exists");
-    	}
-    	
-    	return ResponseEntity
-    			.status(HttpStatus.CREATED) //201
-    			.body(inventoryRepository.save(inventory));
-    }
-    
-    public List<InventoryEntity> getInventoryByCategory(int category_id){
-    	return inventoryRepository.findByItemCategoryCategoryId(category_id);
-    }
-    
-    public ResponseEntity<?> isInventoryExists(String inventoryName){
-    	InventoryEntity inventoryFromDb = inventoryRepository.findByNameIgnoreCase(inventoryName);
-    	
-    	if(inventoryFromDb != null) {
-    		return ResponseEntity
-    				.status(HttpStatus.OK) // 200
-    				.body(inventoryName + " Exists in the database");
-    	}
-    	
-    	return ResponseEntity
-    			.status(HttpStatus.CONFLICT) // 409
-    			.body(inventoryName + " Does not exists in the database");
-    }
-    
-    public ResponseEntity<?> deleteInventory(int id){
-    	Optional<InventoryEntity> inventory = inventoryRepository.findById(id);
-    	if(inventory.isPresent()) {
-    		inventoryRepository.deleteById(id);
-    		return ResponseEntity
-    				.status(HttpStatus.OK) // 200
-    				.body(inventory);
-    	}else {
-    		return ResponseEntity
-    				.status(HttpStatus.NOT_FOUND) // 404
-    				.body(id + " Does not exists");
-    	}
-    }
-    
-    public ResponseEntity<?> updateInventory(int id, InventoryEntity inventoryToUpdate){
-    	InventoryEntity inventoryFromDb = inventoryRepository.findByNameIgnoreCase(inventoryToUpdate.getName());
-    	if(inventoryFromDb != null) {
-    		return ResponseEntity
-    				.status(HttpStatus.CONFLICT) // 409
-    				.body("The name " + inventoryToUpdate.getName() + " already exists");
-    	}
-    	InventoryEntity inventory = new InventoryEntity();
-    	
-    	inventory = inventoryRepository.findById(id).get();
-    		
-    	inventory.setName(inventoryToUpdate.getName());
-    	inventory.setDescription(inventoryToUpdate.getDescription());
-    	inventory.setQuantity(inventoryToUpdate.getQuantity());
-    	
-    	return ResponseEntity
-    			.status(HttpStatus.OK) // 200
-    			.body(inventoryRepository.save(inventory));
-    }
+	public List<InventoryEntity> getAllInventory() {
+		return inventoryRepository.findAll();
+	}
+
+	public ResponseEntity<?> addInventory(InventoryEntity inventory) {
+		InventoryEntity inventoryfromdb = inventoryRepository.findByNameIgnoreCase(inventory.getName());
+
+		if (inventoryfromdb != null) {
+			return ResponseEntity
+					.status(HttpStatus.CONFLICT) // 409
+					.body(inventory.getName() + " Inventory already exists");
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.CREATED) //201
+				.body(inventoryRepository.save(inventory));
+	}
+
+	public List<InventoryEntity> getInventoryByCategory(int category_id) {
+		return inventoryRepository.findByItemCategoryCategoryId(category_id);
+	}
+
+	public ResponseEntity<?> isInventoryExists(String inventoryName) {
+		InventoryEntity inventoryFromDb = inventoryRepository.findByNameIgnoreCase(inventoryName);
+
+		if (inventoryFromDb != null) {
+			return ResponseEntity
+					.status(HttpStatus.OK) // 200
+					.body(inventoryName + " Exists in the database");
+		}
+
+		return ResponseEntity
+				.status(HttpStatus.CONFLICT) // 409
+				.body(inventoryName + " Does not exists in the database");
+	}
+
+	public ResponseEntity<?> deleteInventory(int id) {
+		Optional<InventoryEntity> inventory = inventoryRepository.findById(id);
+		if (inventory.isPresent()) {
+			inventoryRepository.deleteById(id);
+			return ResponseEntity
+					.status(HttpStatus.OK) // 200
+					.body(inventory);
+		} else {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND) // 404
+					.body(id + " Does not exists");
+		}
+	}
+
+//    public ResponseEntity<?> updateInventory(int id, InventoryEntity inventoryToUpdate){
+//    	InventoryEntity inventoryFromDb = inventoryRepository.findByNameIgnoreCase(inventoryToUpdate.getName());
+//    	if(inventoryFromDb != null) {
+//    		return ResponseEntity
+//    				.status(HttpStatus.CONFLICT) // 409
+//    				.body("The name " + inventoryToUpdate.getName() + " already exists");
+//    	}
+//    	InventoryEntity inventory = new InventoryEntity();
+//
+//    	inventory = inventoryRepository.findById(id).get();
+//
+//    	inventory.setName(inventoryToUpdate.getName());
+//    	inventory.setDescription(inventoryToUpdate.getDescription());
+//    	inventory.setQuantity(inventoryToUpdate.getQuantity());
+//
+//    	return ResponseEntity
+//    			.status(HttpStatus.OK) // 200
+//    			.body(inventoryRepository.save(inventory));
+//    }
+//}
+
+	public ResponseEntity<?> updateInventory(int id, InventoryEntity inventoryToUpdate) {
+		Optional<InventoryEntity> inventoryOptional = inventoryRepository.findById(id);
+		if (inventoryOptional.isPresent()) {
+			InventoryEntity inventory = inventoryOptional.get();
+
+			if (inventoryToUpdate.getName() != null && !inventoryToUpdate.getName().isEmpty()) {
+				InventoryEntity inventoryFromDb = inventoryRepository.findByNameIgnoreCase(inventoryToUpdate.getName());
+				if (inventoryFromDb != null && inventoryFromDb.getInventoryId() != id) {
+					return ResponseEntity
+							.status(HttpStatus.CONFLICT) // 409
+							.body("The name " + inventoryToUpdate.getName() + " already exists");
+				}
+				inventory.setName(inventoryToUpdate.getName());
+			}
+
+			if (inventoryToUpdate.getDescription() != null && !inventoryToUpdate.getDescription().isEmpty()) {
+				inventory.setDescription(inventoryToUpdate.getDescription());
+			}
+
+			if (!Float.isNaN(inventoryToUpdate.getQuantity())) {
+				inventory.setQuantity(inventoryToUpdate.getQuantity());
+			}
+
+			return ResponseEntity
+					.status(HttpStatus.OK) // 200
+					.body(inventoryRepository.save(inventory));
+		} else {
+			return ResponseEntity
+					.status(HttpStatus.NOT_FOUND) // 404
+					.body(id + " Does not exist");
+		}
+	}
 }
