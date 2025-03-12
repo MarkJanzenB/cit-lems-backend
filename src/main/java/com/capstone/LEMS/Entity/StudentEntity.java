1c6d110029b885d3f9f0e8b8e77b2234df1a9ba7
NifaNii <jcarlsabejon@gmail.com>
2025-03-12 12:06:46 +0800

    Datasource now use Aiven
    - added /user/message end point API to authorized request. It will not
    use JWT to test if backend is running without logging in.
    - Fix error caused by StudentEntity line 12 where GeneratedValue does
    not have a GenerationType strategy

--- src/main/java/com/capstone/LEMS/Entity/StudentEntity.java
+++ src/main/java/com/capstone/LEMS/Entity/StudentEntity.java
@@ -3,12 +3,13 @@ package com.capstone.LEMS.Entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class StudentEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int student_id;
     @Column(name = "school_id")
     private String school_id;
