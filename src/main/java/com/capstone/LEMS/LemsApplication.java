248e456022d1e14222bdc78508fec93c38f4d74f
NifaNii <jcarlsabejon@gmail.com>
2025-03-12 14:42:13 +0800

    Added KeepRenderAwakeService.java to prevent host from sleeping
    - Added @EnableScheduling annotation in LemsApplication.java

--- src/main/java/com/capstone/LEMS/LemsApplication.java
+++ src/main/java/com/capstone/LEMS/LemsApplication.java
@@ -2,7 +2,9 @@ package com.capstone.LEMS;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class LemsApplication {
 
