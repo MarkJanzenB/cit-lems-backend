05f80f57e4e48248bedd7e33fb14ae035eae761d
John Baclayon <johnmichael.baclayon@cit.edu>
2025-04-25 13:25:41 +0800

    update the TeacherScheduleEntity from java.sql.Time to java.time.LocalTime

--- src/main/java/com/capstone/LEMS/Entity/TeacherScheduleEntity.java
+++ src/main/java/com/capstone/LEMS/Entity/TeacherScheduleEntity.java
@@ -1,25 +1,10 @@
 package com.capstone.LEMS.Entity;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import java.sql.Time;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.PrePersist;
-
+import jakarta.persistence.*;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.Date;
-
-
-
 
 //to be revised
 @Entity
@@ -31,64 +16,51 @@ public class TeacherScheduleEntity {
     private int teacherScheduleId;
     //Time default format is HH:mm:ss
     @Column(name = "start_time")
-    private Time startTime;
+    private LocalTime startTime; // Changed to LocalTime
     @Column(name = "end_time")
-    private Time endTime;
+    private LocalTime endTime;   // Changed to LocalTime
     @Column(name="lab_num")
     private String labNum;
     @Column(name = "date")
-    //Date default format is YYYY-MM-DD
     private Date date;
-//    @Column(name = "teacher_id")
-//    private int teacherId;
-@ManyToOne
-@JoinColumn(name = "teacher_id", nullable = true)
-private UserEntity teacher;
+    @ManyToOne
+    @JoinColumn(name = "teacher_id", nullable = true)
+    private UserEntity teacher;
     @ManyToOne
     @JoinColumn(name = "year_id", nullable = true)
     private YearSectionEntity yearSection;
     @ManyToOne
     @JoinColumn(name = "createdby_id", nullable = true)
     private UserEntity createdBy;
-
     @Column(name = "date_created")
     private LocalDate dateCreated;
-
-
     @OneToOne(mappedBy = "teacherSchedule")
-	private BorrowItemEntity BorrowItemEntity;
-
-    //Add sched_assign_id FK
-    //Add subject_id FK
+    private BorrowItemEntity BorrowItemEntity;
 
     public TeacherScheduleEntity() {
         super();
     }
-    
-   
-    
-    public TeacherScheduleEntity(int teacherScheduleId, Time startTime, Time endTime, String labNum, Date date,
-			UserEntity teacher, YearSectionEntity yearSection, UserEntity createdBy, LocalDate dateCreated) {
-		super();
-		this.teacherScheduleId = teacherScheduleId;
-		this.startTime = startTime;
-		this.endTime = endTime;
-		this.labNum = labNum;
-		this.date = date;
-		this.teacher = teacher;
-		this.yearSection = yearSection;
-		this.createdBy = createdBy;
-		this.dateCreated = dateCreated;
-	}
 
-
-
-	@PrePersist
-    protected void onCreate() {
-    	this.dateCreated = LocalDate.now();
+    public TeacherScheduleEntity(int teacherScheduleId, LocalTime startTime, LocalTime endTime, String labNum, Date date,
+                                 UserEntity teacher, YearSectionEntity yearSection, UserEntity createdBy, LocalDate dateCreated) {
+        super();
+        this.teacherScheduleId = teacherScheduleId;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.labNum = labNum;
+        this.date = date;
+        this.teacher = teacher;
+        this.yearSection = yearSection;
+        this.createdBy = createdBy;
+        this.dateCreated = dateCreated;
     }
 
-	@JsonProperty("teacher_schedule_id")
+    @PrePersist
+    protected void onCreate() {
+        this.dateCreated = LocalDate.now();
+    }
+
+    @JsonProperty("teacher_schedule_id")
     public int getTeacherScheduleId() {
         return teacherScheduleId;
     }
@@ -96,17 +68,17 @@ private UserEntity teacher;
         this.teacherScheduleId = teacherScheduleId;
     }
     @JsonProperty("start_time")
-    public Time getStartTime() {
+    public LocalTime getStartTime() { // Changed to LocalTime
         return startTime;
     }
-    public void setStartTime(Time startTime) {
+    public void setStartTime(LocalTime startTime) { // Changed to LocalTime
         this.startTime = startTime;
     }
     @JsonProperty("end_time")
-    public Time getEndTime() {
+    public LocalTime getEndTime() {   // Changed to LocalTime
         return endTime;
     }
-    public void setEndTime(Time endTime) {
+    public void setEndTime(LocalTime endTime) {     // Changed to LocalTime
         this.endTime = endTime;
     }
     @JsonProperty("lab_num")
@@ -123,43 +95,31 @@ private UserEntity teacher;
     public void setDate(Date date) {
         this.date = date;
     }
-//    @JsonProperty("teacher_id")
-//    public int getTeacherId() {
-//        return teacherId;
-//    }
-@JsonProperty("teacher")
-public UserEntity getTeacher() {
-    return teacher;
-}
-
+    @JsonProperty("teacher")
+    public UserEntity getTeacher() {
+        return teacher;
+    }
     public void setTeacher(UserEntity teacher) {
         this.teacher = teacher;}
-//    public void setTeacherId(int teacherId) {
-//        this.teacherId = teacherId;
-//    }
     @JsonProperty("year_and_section")
     public YearSectionEntity getYearSection() {
         return yearSection;
+    }
+    public void setYearSection(YearSectionEntity yearSection) {
+        this.yearSection = yearSection;
     }
     @JsonProperty("created_by")
     public UserEntity getCreatedBy() {
         return createdBy;
     }
-
     public void setCreatedBy(UserEntity createdBy) {
         this.createdBy = createdBy;
     }
-
     @JsonProperty("date_created")
     public LocalDate getDateCreated() {
         return dateCreated;
     }
-
     public void setDateCreated(LocalDate dateCreated) {
         this.dateCreated = dateCreated;
-    }
-
-    public void setYearSection(YearSectionEntity yearSection) {
-        this.yearSection = yearSection;
     }
 }
